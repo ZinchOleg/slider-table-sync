3af6c0fcff3f44ac6863c4dbf893a9163464784d
ZinchOleg <olegzzzinch@gmail.com>
2023-09-05 11:27:22 +0300

    edit configs for deploy

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
+  base: "/slider-table-sync",
   plugins: [
     react({
       babel: { plugins: ["styled-jsx/babel"] },
