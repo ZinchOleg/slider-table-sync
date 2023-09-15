6d6426f8b4834756aeacb97cd16f8c47110a4b73
ZinchOleg <olegzzzinch@gmail.com>
2023-09-15 19:20:46 +0300

    Responsive for mobile and tablets have been added

--- src/App.tsx
+++ src/App.tsx
@@ -1,75 +1,67 @@
-import { useState } from "react";
+import {useState} from "react";
 import SliderTable from "./components/SliderTable";
 import data from "./mock-data";
-import { Navigation, Pagination, Controller } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import type { Swiper as SwiperType } from 'swiper';
+import {Navigation, Pagination, Controller} from "swiper/modules";
+import {Swiper, SwiperSlide} from "swiper/react";
+import type {Swiper as SwiperType} from 'swiper';
 import SliderImage from "./components/SliderImage";
 
 const App = () => {
-  const [firstSwiper, setFirstSwiper] = useState<SwiperType | null>(null);
-  const [secondSwiper, setSecondSwiper] = useState<SwiperType | null>(null);
+    const [firstSwiper, setFirstSwiper] = useState<SwiperType | null>(null);
+    const [secondSwiper, setSecondSwiper] = useState<SwiperType | null>(null);
 
-  const sliderSettings = {
-    440: {
-      slidesPerView: 1,
-    },
-    680: {
-      slidesPerView: 2,
-    },
-    1024: {
-      slidesPerView: 3,
-    },
-    1440: {
-      slidesPerView: 4,
-    },
-  };
+    const sliderSettings = {
+        440: {
+            slidesPerView: 1,
+        }, 680: {
+            slidesPerView: 2,
+        }, 1024: {
+            slidesPerView: 3,
+        }, 1440: {
+            slidesPerView: 4,
+        },
+    };
 
-  return (
-    <div className="app">
-      <Swiper
-        className="img-slider"
-        onSwiper={setFirstSwiper}
-        controller={{ control: secondSwiper }}
-        modules={[Controller, Navigation, Pagination]}
-        navigation
-        pagination={{clickable: true}}
-        breakpoints={sliderSettings}
-        spaceBetween={12}
-        loop
-      >
-        {
-          data.map((item) => (
-            <SwiperSlide key={item.id}>
-              <SliderImage data={item} />
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
+    return (<div className="app">
+            <div>
+                <Swiper
+                    className="img-slider"
+                    onSwiper={setFirstSwiper}
+                    controller={{control: secondSwiper}}
+                    modules={[Controller, Navigation, Pagination]}
+                    navigation
+                    pagination={{clickable: true}}
+                    breakpoints={sliderSettings}
+                    spaceBetween={12}
+                    loop
+                >
+                    {data.map((item) => (<SwiperSlide key={item.id}>
+                            <SliderImage data={item}/>
+                        </SwiperSlide>))}
+                </Swiper>
 
-      <h1>Comparison table</h1>
+                <h1>Comparison table</h1>
 
-      <Swiper
-        className="info-slider"
-        onSwiper={setSecondSwiper}
-        controller={{ control: firstSwiper }}
-        modules={[Controller, Navigation, Pagination]}
-        pagination={{clickable: true }}
-        breakpoints={sliderSettings}
-        loop
-      >
-        {
-          data.map((item) => (
-            <SwiperSlide key={item.id}>
-              <SliderTable data={item} />
-            </SwiperSlide>
-          ))
-        }
-      </Swiper>
+                <Swiper
+                    className="info-slider"
+                    onSwiper={setSecondSwiper}
+                    controller={{control: firstSwiper}}
+                    modules={[Controller, Navigation, Pagination]}
+                    pagination={{clickable: true}}
+                    breakpoints={sliderSettings}
+                    loop
+                >
+                    {data.map((item) => (<SwiperSlide key={item.id}>
+                            <SliderTable data={item}/>
+                        </SwiperSlide>))}
+                </Swiper>
 
-      <img src="./kisspng-mazda-rx-7-sports-car-mazda-rx-3-rx7-5b4ccb6d4b4ec1.4833586315317594693085.png" className="bg"/>
-    </div>
-  );
+                {/*<img src="./kisspng-mazda-rx-7-sports-car-mazda-rx-3-rx7-5b4ccb6d4b4ec1.4833586315317594693085.png" className="bg"/>*/}
+            </div>
+            <div className="bg">
+                <img src="./kisspng-mazda-rx-7-sports-car-mazda-rx-3-rx7-5b4ccb6d4b4ec1.4833586315317594693085.png"/>
+            </div>
+        </div>);
 }
 
 export default App;
